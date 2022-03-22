2ce44f4a32848f4edc0c616ab3def75359ec0d96
nksin <snandan748@gmail.com>
2022-03-22 21:51:09 +0530

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+<v-app>
+  <v-main>
+  
+  <v-toolbar>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar-title>portfolio</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn text>
+      <n-link id="brown" to="/weather-app">weather app</n-link>
+    </v-btn>
+  </v-toolbar>
+  <v-main>
+    <nuxt/>
+  </v-main>
+  </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+#brown{
+  text-decoration: none;
+  color: rgb(255, 255, 255);
+}
+
+</style>
---
+++ pages/weather-app.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-container>
+      <!-- <v-layout> -->
+        <h1 class="text-h4 text-center">weather app</h1>
+        <v-flex xs12>
+          <v-card color="#455A64" dark>
+            <v-card-text>
+              here card
+            </v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs12 class="mt-4">
+          <v-text-field
+            label="enter city name" solo
+          ></v-text-field>
+        </v-flex>
+      <!-- </v-layout> -->
+    </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      city:'London'
+    }
+  },
+  created(){
+    this.$axios
+    .$get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&
+    appid=ddb956415e4c89646c1fe66f214ec94a`)
+    .then(res => console.log(res))  }
+
+}
+</script>
+
+<style>
+
+</style>
